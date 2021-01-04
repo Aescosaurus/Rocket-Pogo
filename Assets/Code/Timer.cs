0800927c1902d3567a76889b752956dca36b2477
Aescosaurus <michaelbuslik@hotmail.com>
2021-01-04 15:23:43 -0700

    Movement in.

---
+++ Assets/Code/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Timer
+{
+	public Timer( float duration )
+	{
+		this.duration = duration;
+	}
+
+	public bool Update( float dt )
+	{
+		if( curTime <= duration ) curTime += dt;
+
+		return ( IsDone() );
+	}
+
+	public void Reset()
+	{
+		curTime = 0.0f;
+	}
+
+	public bool IsDone()
+	{
+		return ( curTime >= duration );
+	}
+
+	[SerializeField] float duration;
+	float curTime = 0.0f;
+}
