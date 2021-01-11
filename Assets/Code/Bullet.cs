3bdfe9793da6bffbed4610cdd603ea044db5d74b
Aescosaurus <michaelbuslik@hotmail.com>
2021-01-10 17:58:15 -0700

    Stationary shooty enemy.

---
+++ Assets/Code/Bullet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet
+	:
+	MonoBehaviour
+{
+	void Update()
+	{
+		if( lifetime.Update( Time.deltaTime ) )
+		{
+			Destroy( gameObject );
+		}
+	}
+
+	void OnTriggerEnter( Collider coll )
+	{
+		if( coll.GetComponent<PogoMove>() != null )
+		{
+			// todo damage player
+
+			Destroy( gameObject );
+		}
+	}
+
+	[SerializeField] Timer lifetime = new Timer( 5.0f );
+}
---
+++ Assets/Code/EnemyTurret.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurret
+	:
+	MonoBehaviour
+{
+	void Start()
+	{
+		player = FindObjectOfType<PogoMove>().gameObject;
+		turret = transform.GetChild( 0 );
+
+		bulletPrefab = Resources.Load<GameObject>( "Prefabs/Bullet" );
+		shotPos = transform.Find( "Turret/Aimer/ShotPos" );
+	}
+
+	void Update()
+	{
+		var diff = player.transform.position - transform.position;
+
+		if( diff.sqrMagnitude < Mathf.Pow( shootRange,2 ) )
+		{
+			turret.transform.LookAt( player.transform );
+
+			if( shotRefire.Update( Time.deltaTime ) )
+			{
+				shotRefire.Reset();
+
+				var bullet = Instantiate( bulletPrefab );
+				bullet.transform.position = shotPos.position;
+				bullet.transform.rotation = turret.transform.rotation;
+
+				bullet.GetComponent<Rigidbody>().AddForce(
+					bullet.transform.forward * shotSpeed,
+					ForceMode.Impulse );
+			}
+		}
+		else
+		{
+			shotRefire.Reset();
+		}
+	}
+
+	GameObject player;
+	[SerializeField] float shootRange = 30.0f;
+	Transform turret;
+
+	Timer shotRefire = new Timer( 3.0f );
+
+	GameObject bulletPrefab;
+	Transform shotPos;
+	[SerializeField] float shotSpeed = 1.0f;
+}
